d8047ca0229684feb2164c19c215e9c573e60ed7
tdv14011984@gmail.com <Parol2022!-forGIT>
2022-05-06 14:28:56 +0300

    add BeforeAll

--- src/test/java/denis/timushev/TestForFriends.java
+++ src/test/java/denis/timushev/TestForFriends.java
@@ -1,4 +1,23 @@
 package denis.timushev;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class TestForFriends {
+
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.browserSize = "1920x1080";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
 }
