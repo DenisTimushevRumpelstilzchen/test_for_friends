296703485c500e6b4ee6984540a615a13bca7d52
Your NameDenis <tdv14011984@gmail.com>
2022-05-31 15:14:07 +0300

    add owner

--- src/test/java/denis/timushev/TestForFriends.java
+++ src/test/java/denis/timushev/TestForFriends.java
@@ -23,7 +23,8 @@ public class TestForFriends {
 
         Configuration.baseUrl = System.getProperty("pageUrl", "https://demoqa.com");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://"+ config.login() + ":" + config.password() + "@"
+                + System.getProperty("remote", "selenoid.autotests.cloud/wd/hub" );
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
